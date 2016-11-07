2a35dc93f239b269225166c53e4e10ba949cc8c6
yojvn <yojvn@OkuRin-NOTE>
2016-11-07 09:41:14 -0300

    calc

---
+++ ejemploCalculadoraFrame/src/ejemplocalculadoraframe/EjemploCalculadoraFrame.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplocalculadoraframe;
+
+/**
+ *
+ * @author yojvn
+ */
+public class EjemploCalculadoraFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        calculadora calc = new calculadora();
+        calc.setVisible(true);
+        
+    }
+    
+}
---
+++ ejemploCalculadoraFrame/src/ejemplocalculadoraframe/metodosCalculadora.java
@@ -0,0 +1,29 @@
+package ejemplocalculadoraframe;
+import java.util.*;
+/**
+ *
+ * @author yojvn
+ */
+public class metodosCalculadora{
+            
+    public metodosCalculadora(){}
+
+
+    public static long suma(long num1, long num2) {
+        return (num1 + num2);
+    }
+
+    public static long resta(long num1, long num2) {
+        return (num1 - num2);
+    }
+
+    public static double div(double num1, double num2) {
+        return (num1 / num2);
+    }
+
+    public static long mult(long num1, long num2) {
+        return num1 * num2;
+    }
+
+
+}
